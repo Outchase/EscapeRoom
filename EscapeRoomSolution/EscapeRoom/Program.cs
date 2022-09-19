df18a4a43d1a5bc7969d28994a3e0292f80a10bd
Outchase <101480664+Outchase@users.noreply.github.com>
2022-09-19 16:37:55 +0200

    first commit

---
+++ EscapeRoomSolution/EscapeRoom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EscapeRoom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
